4b4c4072e218c80e205670ab2d0365e6880e9c8b
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2023-06-08 20:41:22 -0400

    feat(beecrowd): adiciona a função de leitura das entradas do usuário e mostra a resolução do problema

--- Beecrowd/1828 - Bazinga!/1828.py
+++ Beecrowd/1828 - Bazinga!/1828.py
@@ -1,3 +1,4 @@
+# Função para verificar a reação dos jogadores Sheldon e Raj no jogo pedra, papel e tesoura
 def verificar_reacao_sheldon(escolha_sheldon, escolha_raj):
     if escolha_sheldon == escolha_raj:
         return "De novo!"
@@ -12,3 +13,15 @@ def verificar_reacao_sheldon(escolha_sheldon, escolha_raj):
     else:
         return "Raj trapaceou!"
         
+# Função para ler a entrada e resolver o problema
+def ler_entradas():
+    casos_teste = int(input())  # Lê o número de casos de teste
+    for i in range(1, casos_teste + 1):
+        escolhas = input().split()  # Lê as escolhas de Sheldon e Raj para cada caso de teste
+        escolha_sheldon = escolhas[0]
+        escolha_raj = escolhas[1]
+        resultado = verificar_reacao_sheldon(escolha_sheldon, escolha_raj)  # Verifica a reação de Sheldon
+        print(f"Caso #{i}: {resultado}")  # Imprime a reação correspondente
+
+# Chamada da função para ler a entrada e resolver o problema
+ler_entradas()
