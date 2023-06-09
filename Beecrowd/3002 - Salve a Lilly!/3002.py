438aa63aeb3ad9426ef9cbc20a22d31312e31b68
Jerfferson Júnior <127704416+JerffersonJunior@users.noreply.github.com>
2023-06-08 21:01:41 -0400

    Create 3002.py

---
+++ Beecrowd/3002 - Salve a Lilly!/3002.py
@@ -0,0 +1,13 @@
+import math
+
+def calcular_total_impostos(n):
+    # Encontra o maior divisor de N
+    maior_divisor = 1
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            maior_divisor = max(maior_divisor, i)
+
+    # Calcula o total mínimo de impostos
+    total_impostos = n // maior_divisor
+
+    return total_impostos
