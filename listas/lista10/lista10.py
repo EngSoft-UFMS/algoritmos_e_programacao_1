e5c912c48ed573f965f50bcb8028d08c5b36dfdf
Jerfferson Júnior <127704416+JerffersonJunior@users.noreply.github.com>
2023-06-13 16:00:20 -0400

    feat(listas): adiciona resolução dos exercícios 3 e 4

--- listas/lista10/lista10.py
+++ listas/lista10/lista10.py
@@ -12,3 +12,38 @@ posicao = int(input())
 print(lista[posicao])
 lista.pop(posicao)
 print(lista)
+
+# Resolução do exercício 3
+tam = int(input())
+lista = []
+
+
+cont = 1
+verif = 0
+
+while cont <= tam:
+    num = int(input())
+    lista.append(num)
+    cont = cont + 1
+
+numpverif = int(input())
+
+for i in range(tam):
+    if lista[i] == numpverif:
+        print('sim')
+    else: 
+        verif = verif + 1
+        
+if verif == tam:
+    print('não')
+    
+# Resolução do exercício 4
+prodalcool = list(map(float, input().split()))
+somasemana = 0
+
+for i in range(len(prodalcool)):
+    somasemana = somasemana + prodalcool[i]
+
+print(f'{somasemana:.2f}')
+
+
